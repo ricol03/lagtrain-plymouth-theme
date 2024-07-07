51cd3fa325c9851d9b7c3c24b76e7ec1c66aa275
ricol03 <87026265+ricol03@users.noreply.github.com>
2024-07-07 14:09:23 +0100

    updated files

---
+++ lagtrain/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Lagtrain Plymouth Theme";
+  
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs, ... }: {
+    packages.x86_64-linux.default = 
+    with import nixpkgs { system = "x86_64-linux"; };
+    stdenv.mkDerivation {
+      pname = "plymouth-theme-lagtrain";
+      version = "1.0.0";
+      src = ./lagtrain;
+      dontBuild = true;
+      installPhase = ''
+        mkdir -p $out/share/plymouth/themes/lagtrain
+        cp * $out/share/plymouth/themes/lagtrain
+        find $out/share/plymouth/themes/ -name \*.plymouth -exec sed -i "s@\/usr\/@$out\/@" {} \;
+      '';
+    };
+  };
+}
